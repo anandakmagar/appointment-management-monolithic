66d5a7b286738a2e766e45de793c4c07c1e7b3d2
Ananda Magar <anandakmagar@gmail.com>
2024-08-12 22:21:21 -0600

    logout feature added

--- src/main/java/com/appointment_management/security/SecurityConfig.java
+++ src/main/java/com/appointment_management/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/security/login**","/metrics","/security/logout", "/actuator/prometheus", "/security/send-reset-code**","/security/change-password**","/public/**").permitAll()
+                        .requestMatchers("/security/login**","/metrics","/security/logout**", "/actuator/prometheus", "/security/send-reset-code**","/security/change-password**","/public/**").permitAll()
                         .requestMatchers("/admin/**").hasAuthority("ADMIN")
                         .anyRequest().authenticated()
                 )
